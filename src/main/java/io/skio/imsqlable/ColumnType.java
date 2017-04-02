c3aaaa52de13beb2116d777bda45517e85280aa4
Cameron Mullen <cameron.mullen@gmail.com>
2017-04-02 09:34:15 -0700

    Add ColumnType

---
+++ src/main/java/io/skio/imsqlable/ColumnType.java
@@ -0,0 +1,18 @@
+package io.skio.imsqlable;
+
+public enum ColumnType {
+  BINARY,
+  BOOLEAN,
+  DATE,
+  DATETIME,
+  DECIMAL,
+  FLOAT,
+  INTEGER,
+  BIGINT,
+  PRIMARY_KEY,
+  REFERENCES,
+  STRING,
+  TEXT,
+  TIME,
+  TIMESTAMP
+}
